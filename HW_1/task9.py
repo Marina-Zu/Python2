3cbd885f272128055d1230847fc2ad20bc88b266
Marina_Zu <bugsi-47@yandex.ru>
2023-06-06 20:04:41 +0300

    Add HW_1

---
+++ HW_1/task9.py
@@ -0,0 +1,11 @@
+# Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
+
+for i in range(2, 10):
+    for j in range(2, 6):
+        print(j, "*", i, "=", i * j, end='      ')
+    print()
+print()
+for i in range(2, 10):
+    for j in range(6, 10):
+        print(j, "*", i, "=", i * j, end='      ')
+    print()
